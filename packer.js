88cd0a24404367991294440aad2eeb8e9ebb6703
slpuk <yarik6052@gmail.com>
2026-05-05 16:44:46 +0300

    feat: added deserialization func

--- packer.js
+++ packer.js
@@ -1,6 +1,6 @@
 const zlib = require('zlib');
 const crypto = require('crypto');
-const magic = 182;  // CRC-8-ROHC of Scoperta/1
+const magic = 184;  // CRC-8-ROHC from Scoperta/1
 const id = crypto.randomBytes(2);
 
 function serialize(name) {
@@ -27,5 +27,24 @@ function serialize(name) {
     return Buffer.concat([packetBody, crcBuf]);
 }
 
+function deserialize(packet) {
+    const magic = packet.readUInt8(0);
+    const id = packet.readUInt16BE(1);
+    const nameLength = packet.readUInt16BE(3);
+    const name = packet.subarray(5, 5 + nameLength).toString('utf8');
+    const receivedCrc = packet.readUInt32BE(5 + nameLength);
+
+    return {
+        magic: magic,
+        id: id,
+        nameLength: nameLength,
+        name: name,
+        crc: receivedCrc
+    };
+}
+
 const ser_packet = serialize("Device");
-console.log(packet.toString('hex').toUpperCase());
+console.log(ser_packet.toString('hex').toUpperCase());
+
+const des_packet = deserialize(ser_packet);
+console.log(des_packet);
