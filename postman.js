b82e8037d76c597380fb6ca0073e4853be6a8a22
slpuk <yarik6052@gmail.com>
2026-05-07 18:59:08 +0300

    feat: created socket functions

--- postman.js
+++ postman.js
@@ -4,19 +4,42 @@ const socket = dgram.createSocket('udp4');
 const multicastAddr = '224.0.112.80';
 const multicastPort = 57620;
 
-socket.on('error', (err) => {
-    console.error(`Socket error: ${err.message}`);
-});
+function listen() {
+    socket.on('error', (err) => {
+        console.error(`Socket error: ${err.message}`);
+    });
 
-socket.on('message', (msg, rinfo) => {
-    console.log(`Received a packet from ${rinfo.address}:${rinfo.port}`);
-});
+    socket.on('message', (msg, rinfo) => {
+        if (msg.length < 3) {
+            return;
+        }
 
-socket.bind(multicastPort, () => {
-    socket.addMembership(multicastAddr, '0.0.0.0');
-    console.log(`Scoperta working on ${multicastAddr}:${multicastPort}\n`);
-});
+        const senderId = msg.readUInt16BE(1);
+        console.log(`Received a packet from ${rinfo.address}:${rinfo.port}`);
+        console.log(msg.toString('utf-8'), "\n");
+    });
+
+    socket.bind(multicastPort, () => {
+        socket.addMembership(multicastAddr, '0.0.0.0');
+        console.log(`Scoperta working on ${multicastAddr}:${multicastPort}\n`);
+    });
+}
+
+function send() {
+    socket.send("Hello, World!", multicastPort, multicastAddr);
+}
+
+function close() {
+    try {
+        socket.dropMembership(multicastAddr);
+    } catch (err) {
+        // Nope.
+    }
+    socket.close();
+}
+
+listen();
 
 interval = setInterval(() => {
-    socket.send("Hello, World!", multicastPort, multicastAddr);
+    send();
 }, 2000);
