3d6ff7731e7201256f01e27635d49d67b4bc1ee9
slpuk <yarik6052@gmail.com>
2026-05-07 15:18:50 +0300

    feat: added basic socket funcionality: multicast send & receive

--- postman.js
+++ postman.js
@@ -1,2 +1,22 @@
-// Postman - module for working with UDP sockets
-// Blank.
+const dgram = require('node:dgram');
+const socket = dgram.createSocket('udp4');
+
+const multicastAddr = '224.0.112.80';
+const multicastPort = 57620;
+
+socket.on('error', (err) => {
+    console.error(`Socket error: ${err.message}`);
+});
+
+socket.on('message', (msg, rinfo) => {
+    console.log(`Received a packet from ${rinfo.address}:${rinfo.port}`);
+});
+
+socket.bind(multicastPort, () => {
+    socket.addMembership(multicastAddr, '0.0.0.0');
+    console.log(`Scoperta working on ${multicastAddr}:${multicastPort}\n`);
+});
+
+interval = setInterval(() => {
+    socket.send("Hello, World!", multicastPort, multicastAddr);
+}, 2000);
