66690b074452981f8c50474001ca473bbfb9ce4c
slpuk <yarik6052@gmail.com>
2026-05-09 12:45:53 +0300

    feat: created class Postman

--- postman.js
+++ postman.js
@@ -1,45 +1,53 @@
 const dgram = require('node:dgram');
-const socket = dgram.createSocket('udp4');
 
-const multicastAddr = '224.0.112.80';
-const multicastPort = 57620;
-
-function listen() {
-    socket.on('error', (err) => {
-        console.error(`Socket error: ${err.message}`);
-    });
-
-    socket.on('message', (msg, rinfo) => {
-        if (msg.length < 3) {
-            return;
-        }
-
-        const senderId = msg.readUInt16BE(1);
-        console.log(`Received a packet from ${rinfo.address}:${rinfo.port}`);
-        console.log(msg.toString('utf-8'), "\n");
-    });
-
-    socket.bind(multicastPort, () => {
-        socket.addMembership(multicastAddr, '0.0.0.0');
-        console.log(`Scoperta working on ${multicastAddr}:${multicastPort}\n`);
-    });
-}
-
-function send() {
-    socket.send("Hello, World!", multicastPort, multicastAddr);
-}
-
-function close() {
-    try {
-        socket.dropMembership(multicastAddr);
-    } catch (err) {
-        // Nope.
+class Postman {
+    constructor(id, packet) {
+        this.packet = packet;
+        this.socket = dgram.createSocket('udp4');
+        this.multicastAddr = '224.0.112.80';
+        this.multicastPort = 57620;
+        this.id = id.readUInt16BE(0);
     }
-    socket.close();
+
+    listen() {
+        this.socket.on('error', (err) => {
+            console.error(`Socket error: ${err.message}`);
+        });
+
+        this.socket.on('message', (msg, rinfo) => {
+            if (msg.length < 3) {
+                return;
+            }
+
+            const senderId = msg.readUInt16BE(1);
+            if (senderId != this.id) {
+                console.log(`Received a packet from ${rinfo.address}:${rinfo.port}`);
+                this.onMessage?.(msg, rinfo);
+            }
+        });
+
+        this.socket.bind(this.multicastPort, () => {
+            this.socket.addMembership(this.multicastAddr, '0.0.0.0');
+            console.log(`Scoperta working on ${this.multicastAddr}:${this.multicastPort}\n`);
+        });
+    }
+
+    send() {
+        this.socket.send(this.packet, this.multicastPort, this.multicastAddr);
+    }
+
+    onReceive(callback) {
+        this.onMessage = callback;
+    }
+
+    close() {
+        try {
+            this.socket.dropMembership(this.multicastAddr);
+        } catch (err) {
+            // Nope.
+        }
+        this.socket.close();
+    }
 }
 
-listen();
-
-interval = setInterval(() => {
-    send();
-}, 2000);
+module.exports = Postman;
