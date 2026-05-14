70dcaebcf9335b4b414718fd953af0261fe3e92b
slpuk <yarik6052@gmail.com>
2026-05-14 14:41:49 +0300

    feat: added api

--- index.js
+++ index.js
@@ -1,11 +1,81 @@
-const crypto = require('crypto');
 const Packer = require('./packer');
+const Postman = require('./postman');
+const crypto = require('crypto');
 
-const device_id = crypto.randomBytes(2);
-packer = new Packer("Scoperta/1", device_id);
+class Scoperta {
+    constructor(deviceName) {
+        if (!deviceName || deviceName.length > 1000) {
+            throw new Error('Device name is required and must be <= 1000 bytes');
+        }
+        
+        this.deviceName = deviceName;
+        this.version = "Scoperta/1";
+        this.deviceId = crypto.randomBytes(2);
+        this.packer = new Packer(this.version, this.deviceId);
+        this.packet = this.packer.pack(deviceName);
+        this.postman = new Postman(this.deviceId, this.packet);
+        this.discoveredDevices = new Map();
+    }
 
-const ser_packet = packer.pack("Device");
-console.log("Serialized Packet:", ser_packet.toString('hex').toUpperCase());
+    start() {
+        this.postman.onReceive((msg, rinfo) => {
+            try {
+                const device = this.packer.unpack(msg);
+                
+                this.discoveredDevices.set(device.id, {
+                    id: device.id,
+                    name: device.name,
+                    address: rinfo.address,
+                    port: rinfo.port,
+                    lastSeen: Date.now()
+                });
+                
+                if (this.onDiscovery) {
+                    this.onDiscovery(device, rinfo);
+                }
+            } catch (err) {
+                if (this.onError) {
+                    this.onError(err);
+                }
+            }
+        });
+        
+        this.postman.listen();
+        this.postman.send();
+        
+        this.interval = setInterval(() => {
+            this.postman.send();
+        }, 10000);
+        
+        return this;
+    }
 
-const deser_packet = packer.unpack(ser_packet);
-console.log("Deserialized Packet:", deser_packet);
+    stop() {
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
+        this.postman.close();
+        return this;
+    }
+
+    getDevices() {
+        const now = Date.now();
+        for (const [id, device] of this.discoveredDevices) {
+            if (now - device.lastSeen > 30000) {
+                this.discoveredDevices.delete(id);
+            }
+        }
+        return Array.from(this.discoveredDevices.values());
+    }
+
+    on(event, callback) {
+        if (event === 'discovery') {
+            this.onDiscovery = callback;
+        } else if (event === 'error') {
+            this.onError = callback;
+        }
+        return this;
+    }
+}
+
+module.exports = Scoperta;
